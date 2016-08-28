a3b272b90d7e1facc9e9aca7f69e28ddc0d74f9c
Simon VANDPUTTE <Aitux@users.noreply.github.com>
2016-08-28 14:07:31 +0200

    Ajout des images à la liste et de la classe BarreScore (still WIP)

--- JS3B/src/Monde.java
+++ JS3B/src/Monde.java
@@ -3,7 +3,7 @@ import java.awt.Toolkit;
 public class Monde {
 	public  static int HAUTEUR = 10;
 	public  static int LONGUEUR = 20;
-	static String[] imagesjeu={"ciel.png","cielnuageux.png","oiseaux.png","terre.png","hero.png","avionNazi.png","tankNazi.png","croixKKK.png","membreKKK.png","avionTerro.png","terrorist.png","maison1.png","maison2.png","maison3.png","sable.png","bitume.png","usine.png","palmier.png"};
+	static String[] imagesjeu={"ciel.png","cielnuageux.png","oiseaux.png","terre.png","hero.png","avionNazi.png","tankNazi.png","croixKKK.png","membreKKK.png","avionTerro.png","terrorist.png","maison1.png","maison2.png","maison3.png","sable.png","bitume.png","usine.png","palmier.png","0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","be.png","st.png"};
 	
 	Terrain terrain;
 	SuperPlateau p;
