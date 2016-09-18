e4460bc4f9107121599fd3a853e009520c6545eb
Azzerood <azzedine.aitelhaj@gmail.com>
2016-09-18 17:42:41 +0200

    Modif cinematique

--- JS3B/src/Cinematique.java
+++ JS3B/src/Cinematique.java
@@ -1,6 +1,8 @@
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Cinematique extends JFrame {
@@ -13,10 +15,17 @@ public class Cinematique extends JFrame {
 	}
 	public void defiler(){
 		for(int i =0;i<images.length;i++){
-			ImagePanel panel = new ImagePanel(new ImageIcon(images[i]).getImage());
+			ImagePanel panel=null;
+			try {
+				panel = new ImagePanel(ImageIO.read(new File(images[i]))  );
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			System.out.println(images[i]);
+			if(panel != null){
 			this.getContentPane().add(panel);
-			
+			}
 			
 			try {
 				Thread.sleep(6000);
