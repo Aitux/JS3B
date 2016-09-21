aee925d2d9dca81a2d2f3fde63d34cbc36d6cf91
azzed <azzed@DESKTOP-3JA9ICU>
2016-09-21 22:01:30 +0200

    nouveau sons oiseaux et ennemie

--- JS3B/src/Deplacement.java
+++ JS3B/src/Deplacement.java
@@ -1,5 +1,12 @@
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Deplacement {
 
@@ -53,11 +60,43 @@ public class Deplacement {
 		if(monde.terrain.tableau[monde.terrain.perso.getAbscisse()][monde.terrain.perso.getOrdonnee()].getElement() instanceof Ciel ){
 			if( ((Ciel) (monde.terrain.tableau[monde.terrain.perso.getAbscisse()][monde.terrain.perso.getOrdonnee()].getElement())).isOiseaux() ){
 				monde.terrain.perso.setNbVies(monde.terrain.perso.getNbVies()-1);
+				 try {
+			         // Open an audio input stream.
+			         URL url = this.getClass().getClassLoader().getResource("oiseau.wav");
+			         AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+			         // Get a sound clip resource.
+			         Clip clip = AudioSystem.getClip();
+			         // Open audio clip and load samples from the audio input stream.
+			         clip.open(audioIn);
+			         clip.start();
+			      } catch (UnsupportedAudioFileException e) {
+			         e.printStackTrace();
+			      } catch (IOException e) {
+			         e.printStackTrace();
+			      } catch (LineUnavailableException e) {
+			         e.printStackTrace();
+			      }
 				scoreN --;
 			}
 			if(monde.terrain.tableau[monde.terrain.perso.getAbscisse()][monde.terrain.perso.getOrdonnee()].getEnnemi() != null){
 				monde.terrain.perso.setNbVies(monde.terrain.perso.getNbVies()-1);
 				scoreN --;
+				 try {
+			         // Open an audio input stream.
+			         URL url = this.getClass().getClassLoader().getResource("ennemi.wav");
+			         AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+			         // Get a sound clip resource.
+			         Clip clip = AudioSystem.getClip();
+			         // Open audio clip and load samples from the audio input stream.
+			         clip.open(audioIn);
+			         clip.start();
+			      } catch (UnsupportedAudioFileException e) {
+			         e.printStackTrace();
+			      } catch (IOException e) {
+			         e.printStackTrace();
+			      } catch (LineUnavailableException e) {
+			         e.printStackTrace();
+			      }
 			}
 		}
 	}
